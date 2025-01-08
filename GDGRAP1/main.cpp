1ed52fb030a2e005787fcb70242e5341b8415d29
peter-abada <peter_abada@dlsu.edu.ph>
2025-01-08 16:01:25 +0800

    Test push
    Added second name for testing purposes

--- GDGRAP1/main.cpp
+++ GDGRAP1/main.cpp
@@ -9,7 +9,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Peter Abada", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Peter Gabriel Abada", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
