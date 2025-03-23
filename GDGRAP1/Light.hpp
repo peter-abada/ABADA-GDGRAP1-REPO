7031fb79f151cd055e3cafa9bab45b555fee48f0
notmarcovaldez <marco_valdez@dlsu.edu.ph>
2025-03-23 20:08:04 +0800

    test

--- GDGRAP1/Light.hpp
+++ GDGRAP1/Light.hpp
@@ -2,22 +2,15 @@
 #define LIGHT_HPP
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 class Light {
-
 protected:
-	glm::vec3 lightPos;
-	glm::vec3 lightColor;
+    glm::vec3 lightColor;
 
 public:
-	Light(glm::vec3 pos, glm::vec3 color);
-	
-	glm::vec3 getLightPos();
-	void setLightPos(glm::vec3 pos);
-
-	glm::vec3 getLightColor();
-	void setLightColor(glm::vec3 color);
+    Light(glm::vec3 color);
+    glm::vec3 getLightColor();
+    void setLightColor(glm::vec3 color);
 };
 
 #endif
